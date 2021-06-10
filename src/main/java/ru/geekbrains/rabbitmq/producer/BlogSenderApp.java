261fde500191b91b97f2c56a796a54da18f956f1
KatySid <whitekaty666@gmail.com>
2021-06-11 01:23:31 +0400

    todo TOPIC

--- src/main/java/ru/geekbrains/rabbitmq/producer/BlogSenderApp.java
+++ src/main/java/ru/geekbrains/rabbitmq/producer/BlogSenderApp.java
@@ -8,22 +8,20 @@ import com.rabbitmq.client.ConnectionFactory;
 import java.util.Scanner;
 
 public class BlogSenderApp {
-    private static final String EXCHANGE_NAME = "Blog";
-
+    private static final String EXCHANGE_NAME = "Blog1";
     public static void main(String[] args) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         try (Connection connection = factory.newConnection();
              Channel channel = connection.createChannel()) {
-            channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
-
+            channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.TOPIC);
             while(true) {
                 String text = writeText();
                 String[] token = text.split("\\s");
                 String topic = token[0];
-                String message = token[1];
+                String message = token[1]+" ";
                 for (int i = 2; i < token.length; i++) {
-                   message = message+token[i]+" ";
+                   message = message +token[i]+" ";
                 }
                 channel.basicPublish(EXCHANGE_NAME, topic, null, message.getBytes("UTF-8"));
                 System.out.println(" [x] Sent message '" + message + "'");
@@ -33,6 +31,10 @@ public class BlogSenderApp {
     public static String writeText (){
         Scanner sc = new Scanner(System.in);
         String readCode=sc.nextLine();
+        while(readCode.split("\\s").length<=1){
+            System.out.println("Enter topic and content");
+            readCode= sc.nextLine();
+        }
         return readCode;
     }
 }
